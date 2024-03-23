73929ab65586c95e4f0ecd1329973c9f8476ba97
Thomas <thomasabraham832@gmail.com>
2024-03-23 23:00:49 +0000

    modularize

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
-// Script Name: Auto Splitser for Internet
+// Script Name: Auto Splitser for Rent
 
 const puppeteer = require('puppeteer');
+const prompt = require("prompt-sync")();
 
-(async () => {
+async function splitStuff(thing,cost) {
     // Launch the browser and open a new blank page
-    const browser = await puppeteer.launch({ headless: true, args: ['--start-maximized','--incognito']});
+    const browser = await puppeteer.launch({ headless: false, args: ['--start-maximized','--incognito']});
     const page = await browser.newPage();
     console.log(await browser.userAgent());
 
@@ -15,7 +16,7 @@ const puppeteer = require('puppeteer');
 	    })
     };
 
-    // Navigate the page to a URL
+    // Navigate to Splitser Lists
     await page.goto(`https://app.splitser.com/lists`, { waitUntil: 'networkidle0' });
     console.log("Navigation to splitser.com : SUCCESS");
     await delay(500);
@@ -24,9 +25,9 @@ const puppeteer = require('puppeteer');
     await page.setViewport({width: 1080, height: 1024});
 
     // Login to Splitser
-    await page.type('#__next > div.content-app > div > div > form > div:nth-child(1) > div > input', '<enter-email-id>', {delay: 100}); // Enter Email
+    await page.type('#__next > div.content-app > div > div > form > div:nth-child(1) > div > input', 'thomasabraham832+rentbotsplitser@gmail.com', {delay: 100}); // Enter Email
     console.log("Email Entered : SUCCESS");
-    await page.type('#__next > div.content-app > div > div > form > div:nth-child(2) > div > input', '<enter-password>', {delay: 100}); // Enter Password
+    await page.type('#__next > div.content-app > div > div > form > div:nth-child(2) > div > input', 'RKmJVoQQBBcKqJ9oK4FF', {delay: 100}); // Enter Password
     console.log("Password Entered : SUCCESS");
     const loginAction = '#login-form-submit > button > div.WButton__CustomContainer-sc-1o64yp6-0.idKCuO'; // Selector for Login Button
     await page.click(loginAction);
@@ -64,20 +65,24 @@ const puppeteer = require('puppeteer');
     ];
     const nextMonth = months[new Date().getMonth()+1];
 
-    // Create Internet Entry
-    await page.type('#transaction-calculator','<enter-amount>', {delay: 100}); // Enter Internet Amount
+    // Create Rent Entry
+    await page.type('#transaction-calculator',cost, {delay: 100}); // Enter Rent Amount
     await page.select('.WMemberDropdown__SelectContainer-sc-1atdj3f-2', 'b8cc017f-70e3-4bee-bb90-a08acde56637'); // Select Thomas in the dropdown
-    await page.type('#transaction-what-for', 'Internet for '+nextMonth, {delay: 100}); // Enter Description
+    await page.type('#transaction-what-for', thing+' for '+nextMonth, {delay: 100}); // Enter Description
     const splitButton = '#__next > div.content-app > div.CreateUpdate__FullHeight-sc-1qhn6my-0.cjECzb > div.Page__BackgroundContainer-sc-7q8ic7-0.egZtgT > div > div.Participants__ParticipantsContainer-sc-az6kve-4.cVsIox > div.Participants__TitleContainer-sc-az6kve-0.ceWbIg > div.Participants__DesktopVisibleContainer-sc-az6kve-6.dogmGD > div > button > div.WButton__CustomContainer-sc-1o64yp6-0.idKCuO'; // Selector for Split Equally Button
     await page.click(splitButton);
     const botRemove = '#__next > div.content-app > div.CreateUpdate__FullHeight-sc-1qhn6my-0.cjECzb > div.Page__BackgroundContainer-sc-7q8ic7-0.egZtgT > div > div.Participants__ParticipantsContainer-sc-az6kve-4.cVsIox > div.WParticipants__Container-sc-nr3xs5-0.gXRUeD > div:nth-child(1) > div:nth-child(2) > div.WMultiplier__Container-sc-1yu5ib2-0.jWjfFR > div:nth-child(1) > div'; // Selector for Minus Button
     await page.click(botRemove); // Removes the Bot from the Transaction
     const doneButton = '#__next > div.content-app > div.CreateUpdate__FullHeight-sc-1qhn6my-0.cjECzb > div.CreateUpdate__ActionContainer-sc-1qhn6my-2.hREKlw > div.WButton__ButtonContainer-sc-1o64yp6-2.fhcjlX.CreateUpdate__WCustomDoneButton-sc-1qhn6my-5.fOAQnQ > button > div.WButton__CustomContainer-sc-1o64yp6-0.idKCuO'; // Selector for Done Button
-    await page.click(doneButton);
-    console.log("Created Entry for Internet : SUCCESS");
-    await delay(500);
+    // await page.click(doneButton);
+    console.log("Created Entry for Rent : SUCCESS");
+    await delay(5000);
     console.clear();
 
     await browser.close();
 
-})();
+}
+
+var inputThing = prompt("What are you paying for? ");
+var inputCost = prompt("How much did it cost? ");
+splitStuff(inputThing,inputCost);
--- rent_internet.sh
+++ rent_internet.sh
@@ -1,7 +1,6 @@
 # Bash script to run auto-splister for Rent & Internet
 
 echo "Running Auto-Splitser Script for Rent:"
-node rent.js
-
-echo "Running Auto-Splitser Script for Internet:"
-node internet.js
+node index.js
+echo "Rent"
+echo "2100"
