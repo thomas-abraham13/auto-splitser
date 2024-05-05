bc27fa7250b090db7a593c5088f53221ce1d2f44
Thomas <thomasabraham832@gmail.com>
2024-05-05 22:36:04 +0100

    change console mesage

--- script.sh
+++ script.sh
@@ -1,4 +1,4 @@
 # Bash script to run auto-splister for Rent & Internet
 
-echo "Running Auto-Splitser Script for Rent:"
+echo "Running Auto-Splitser Script:"
 node -e 'require("./index").splitStuff("Stuff","50")'
